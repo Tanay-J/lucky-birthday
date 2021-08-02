b966c2cf812ef76cdad3daf2a7e12a0b2098f4db
Tanay <tanay.joshi9@gmail.com>
2021-08-02 11:40:34 +0530

    changes in logic

--- app.js
+++ app.js
@@ -7,26 +7,25 @@ btn.addEventListener("click",clickHandler);
 
 
 function clickHandler(){
-    var dobArray = dob.value.toString().split('');
+    var dobArray = [];
     var digit = 0;
-    var resultColor = getComputedStyle(document.body).getPropertyValue('--resultColor');
     
+    dobArray = dob.value.toString().split('');
     dobArray.map(item => {
-        if(item!="/"){
+        if(item!="-"){
             digit = parseInt(digit)+parseInt(item); 
         }
        
     })
-    console.log(dobArray)
     if(digit%luckyNo.value == 0 && dobArray.length == 10){
         result.textContent = "Your birthday is LUCKY";
         document.documentElement.style.setProperty('--resultColor','green');
-    }else if(isNaN(digit%luckyNo.value) || dobArray.length != 10){
-        document.documentElement.style.setProperty('--resultColor','black');
-        result.textContent = "Input incorrect";
-    }
-    else{
+    }else if(luckyNo.value && dobArray.length == 10){
         document.documentElement.style.setProperty('--resultColor','orangered');
         result.textContent = "Your birthday is NOT LUCKY";
+    }else {
+        document.documentElement.style.setProperty('--resultColor','black');
+        result.textContent = "All fields are required";
+   
     }
 }
